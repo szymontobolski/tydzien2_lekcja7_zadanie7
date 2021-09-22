ee2088c6ebad28559e9e98aabbe43cad9475fd19
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-22 11:48:15 +0200

    Add project files.

---
+++ tydzien2_lekcja7_zadanie7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tydzien2_lekcja7_zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 63;
+            int c = 79;
+
+            if (c > b && c > a) ;
+            Console.WriteLine($"{79} jest największa z podanych");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
